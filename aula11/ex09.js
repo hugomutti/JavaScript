c839340d4b0bacd739f48a443120f9e4a9e45f78
Hugo Mutti <hugomutticruz@gmail.com>
2024-08-16 09:47:24 -0300

    aula11js

---
+++ aula11/ex09.js
@@ -0,0 +1,13 @@
+var vel = 55.5
+if (vel > 60) {
+    console.log('Multado')
+} else {
+    console.log('Dirija com cuidado e use o cinto!')
+}
+
+var country = 'Brasil'
+if (country != 'Brasil'){
+    console.log('Você é estrangeiro!')
+} else{
+    console.log('Você é brasileiro')
+}
